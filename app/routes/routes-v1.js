188adcb6dc2915dfe79e2c7e79a1dbea0f5a7a34
James Francis <jfranciswebdesign@users.noreply.github.com>
2025-08-27 09:38:28 +0100

    Update routes-v1.js

--- app/routes/routes-v1.js
+++ app/routes/routes-v1.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 
 // Enter your email address to sign in to your GOV.UK One Login
-router.post('/v1/sign-in', function (req, res) {
+router.post('/V1/sign-in', function (req, res) {
   const emailAddress = req.session.data['email-address'];
 
   let errors = {};
@@ -21,74 +21,74 @@ router.post('/v1/sign-in', function (req, res) {
 
   if (Object.keys(errors).length > 0) {
     // Re-render the same page with errors
-    res.render('v1/sign-in', { errors });
+    res.render('V1/sign-in', { errors });
   } else {
     // Continue to the password page
-    res.redirect('/v1/enter-password');
+    res.redirect('/V1/enter-password');
   }
 });
 
 // Company number
-router.post('/v1/company-number', function (request, response) {
-    response.redirect("/v1/view-company-info");
+router.post('/V1/company-number', function (request, response) {
+    response.redirect("/V1/view-company-info");
 });
 
 
 
 // View company info
-router.post('/v1/view-company-info', function (request, response) {
-    response.redirect("/v1/company-authentication");
+router.post('/V1/view-company-info', function (request, response) {
+    response.redirect("/V1/company-authentication");
 });
 
 
 // Company authentication
-router.post('/v1/company-authentication', function (request, response) {
-    response.redirect("/v1/which-director-are-you");
+router.post('/V1/company-authentication', function (request, response) {
+    response.redirect("/V1/which-director-are-you");
 });
 
 
 // Which director are you?
-router.post('/v1/which-director-are-you', function (request, response) {
+router.post('/V1/which-director-are-you', function (request, response) {
   
   var companyNumber = request.session.data['companyNumber'];
   var whichDirectorAreYou = request.session.data['whichDirectorAreYou'];
 
   if (companyNumber === "12345678") {
     if (whichDirectorAreYou === "JaneDoe") {
-      response.redirect("/v1/check-your-answers-single-director");
+      response.redirect("/V1/check-your-answers-single-director");
     } else if (whichDirectorAreYou === "iAmNotADirectorOfThisCompany") {
-      response.redirect("/v1/how-will-the-single-director-be-signing");
+      response.redirect("/V1/how-will-the-single-director-be-signing");
     }
   } else {
-    response.redirect("/v1/which-directors-will-be-signing");
+    response.redirect("/V1/which-directors-will-be-signing");
   }
 });
 
 // Check your answers (single director)
-router.post('/v1/check-your-answers-single-director', function (request, response) {
-    response.redirect("/v1/sign-the-application");
+router.post('/V1/check-your-answers-single-director', function (request, response) {
+    response.redirect("/V1/sign-the-application");
 });
 
 // Sign the application
-router.post('/v1/sign-the-application', function (request, response) {
-    response.redirect("/v1/review-your-payment");
+router.post('/V1/sign-the-application', function (request, response) {
+    response.redirect("/V1/review-your-payment");
 });
 
 // Sign in to CH
-router.post('/v1/sign-in-to-ch', function (request, response) {
-    response.redirect("/v1/gov-one-log-in");
+router.post('/V1/sign-in-to-ch', function (request, response) {
+    response.redirect("/V1/gov-one-log-in");
 });
 
 // Review your payment
-router.post('/v1/review-your-payment', function (request, response) {
-    response.redirect("/v1/how-do-you-want-to-pay");
+router.post('/V1/review-your-payment', function (request, response) {
+    response.redirect("/V1/how-do-you-want-to-pay");
 });
 
 
 
 //Which directors will be signing the application?
-router.post('/v1/which-directors-will-be-signing', function (request, response) {
-    response.redirect("/v1/how-will-the-multiple-directors-be-signing");
+router.post('/V1/which-directors-will-be-signing', function (request, response) {
+    response.redirect("/V1/how-will-the-multiple-directors-be-signing");
 });
 
 module.exports = router;
