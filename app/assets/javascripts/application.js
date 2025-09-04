5492c75f0b84b2cd4ecffaf46e92a7595e0536fe
James Francis <jfranciswebdesign@users.noreply.github.com>
2025-09-04 10:10:18 +0100

    Added a script which stops links from reloading the page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,5 +4,18 @@
 //
 
 window.GOVUKPrototypeKit.documentReady(() => {
-  // Add JavaScript here
+
+
+// Prevents inactive hyperlinks (those with a href of #) from reloading and jumping to the top of the page)
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('a[href="#"]').forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+    });
+  });
+});
+
+
+
+
 })
